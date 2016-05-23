e5f6f42769387d68bad45af7a69bd669a3832f13
Matou <Matou@AzureWave>
2016-05-23 13:21:43 +0200

    Phase 2

---
+++ src/jpu2016/dogfight/controller/UserOrder.java
@@ -0,0 +1,16 @@
+package jpu2016.dogfight.controller;
+
+public class UserOrder {
+	private int player;
+	
+	public void UserOrder(int player, Order order){
+		
+	}
+	public Order getOrder(){
+		return null;
+	}
+	public int getPlayer(){
+		return player;
+		
+	}
+}
